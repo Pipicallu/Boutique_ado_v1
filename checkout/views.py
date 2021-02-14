7823f4c0dad7cf00052bc789a21e3be60cc944b0
Pipicallu <thomas.vassallo@gmail.com>
2021-02-14 15:31:11 +0000

    completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from .models import OrderLineItem, Order
+from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
 
@@ -17,16 +17,16 @@ def cache_checkout_data(request):
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dumps(request.session.get('bag',{})),
+            'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
-            'username':request.user,
-
+            'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot \
-            be processed right now. Please try again later.')
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -36,25 +36,26 @@ def checkout(request):
         bag = request.session.get('bag', {})
 
         form_data = {
-            "full_name": request.POST['full_name'],
-            "email": request.POST['email'],
-            "phone_number": request.POST['phone_number'],
-            "country": request.POST['country'],
-            "post_code": request.POST['postcode'],
-            "full_name": request.POST['full_name'],
-            "town_or_city": request.POST['town_or_city'],
-            "street_address1": request.POST['street_address1'],
-            "street_address2": request.POST['street_address2'],
-            "county": request.POST['county'],
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
         }
-
-        order_form = OrderForm(form_data) # new instance of OrderForm
+        order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
-                    # This is checking if the item_data is an integer and identifying that the product doesn't have a size.
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
                             order=order,
@@ -65,24 +66,25 @@ def checkout(request):
                     else:
                         for size, quantity in item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=quantity,
-                            product_size=size,
-                        )
-                        order_line_item.save()
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag was not found in our database."
-                        "Please Call us for assistance!")
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
-            request.session['save_info'] = 'save_info' in request.POST
-            return redirect(reverse('checkout_success',args=[order.order_number] ))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            messages.error(request,'there was an error with your form. \
-                please double check your information.')
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
@@ -113,21 +115,23 @@ def checkout(request):
 
     return render(request, template, context)
 
+
 def checkout_success(request, order_number):
     """
-    Handle Successdful Checkouts
+    Handle successful checkouts
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order_successfully processed! \
-        your order nuumber is {order_number}. A confirmation email will be sent \
-            to {order.email}')
-    
-    if 'bag' in request.session['bag']:
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
         del request.session['bag']
 
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
     }
+
     return render(request, template, context)
