62f9e0102ef7cee866f20b8c636aca0277c9d6a6
Pipicallu <thomas.vassallo@gmail.com>
2021-02-15 16:07:23 +0000

    updated allauth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
 
-
-
 class UserProfile(models.Model):
     """
-    A user profile for maintaining default delivery information 
-    and order history
+    A user profile model for maintaining default
+    delivery information and order history
     """
-    default_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
@@ -22,8 +20,9 @@ class UserProfile(models.Model):
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
 
-def __str__(self):
-    return self.user.username
+    def __str__(self):
+        return self.user.username
+
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
