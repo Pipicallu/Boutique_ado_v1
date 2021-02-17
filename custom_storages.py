462c8a6ee040114921a38466ce7ef1cbab114d94
Pipicallu <thomas.vassallo@gmail.com>
2021-02-17 15:46:37 +0000

    added AWS

---
+++ custom_storages.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from storages.backends.s3boto3 import s3boto3Storage
+
+class StaticStorage(s3boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+class MediaStorage(s3boto3Storage):
+    location = settings.STATICFILES_LOCATION
