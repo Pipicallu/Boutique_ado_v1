bf5d6cdfe2137abe4f003fedc32aa7d2bccdb797
Pipicallu <thomas.vassallo@gmail.com>
2021-02-17 13:42:31 +0000

    connected to remote database

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,11 +122,15 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://trtniotziqohkw:da4bab26afc784db26b8e1fb0a1d5d0dc16a1e3f02283ccb9f4d5d0dd79c8d01@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/d1v9qb2bj0h3ch')
 }
 
 
